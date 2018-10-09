e3ebfc13fee4ae9c2c2dc9602d44885868a66a62
351157 <351157@0338S-S-R0B6H0G.sdcs.local>
2018-10-09 14:25:14 -0700

    number guessing ip

---
+++ src/NumberGuessingGame.java
@@ -0,0 +1,30 @@
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+
+public class NumberGuessingGame {
+
+	public static void main(String[] args) {
+	int r = 0 ;
+	
+	Random number = new Random();
+	r = number.nextInt(3);
+	
+	String num = JOptionPane.showInputDialog("Choose a number inbetween 0 and 3");
+	int barry = Integer.parseInt(num);
+	if(barry == r ) {
+		JOptionPane.showMessageDialog(null, "Good job. You guessed it right!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "sorry buddy");
+		String input = JOptionPane.showInputDialog("Wanna try again");
+		if(input.equals("yes")) {
+			i--;
+		}
+	}
+	
+	
+}
+}
