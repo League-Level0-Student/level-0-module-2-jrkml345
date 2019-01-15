6ecab60d39504b04428d4d9d2cb690e2eb7e89b3
jrkml345 <37815821+jrkml345@users.noreply.github.com>
2019-01-15 14:25:22 -0800

    Add files via upload

--- src/yearsAlive.java
+++ src/yearsAlive.java
@@ -1,17 +1,13 @@
+package loops;
 
 public class yearsAlive {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-			
-	int i = (2004);
-	for (i = 2004; i<2020; i++) {
-				
-				System.out.println(i);
-				
-				
-					
-				
-				
-			}
-}
+		int i = (2004);
+		for (i = 2004; i < 2020; i++) {
+
+			System.out.println(i);
+
+		}
+	}
 }
